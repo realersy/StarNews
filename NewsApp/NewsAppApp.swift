b737418968cc6434651ab1cfcd76bc7a7814f032
Ersan Shimshek <ersanshimshek1@gmail.com>
2023-02-10 12:51:48 -0500

    Initial Commit

---
+++ NewsApp/NewsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsAppApp.swift
+//  NewsApp
+//
+//  Created by Ersan Shimshek on 10.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
